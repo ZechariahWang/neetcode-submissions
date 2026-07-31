032d6b67b2a2bb1a2f3120551e3232ac5105a5e3
Zechariah Wang <97078224+ZechariahWang@users.noreply.github.com>
2026-07-31 14:16:18 -0400

    Add: remove-node-from-end-of-linked-list - submission-2

---
+++ Data Structures & Algorithms/remove-node-from-end-of-linked-list/submission-2.py
@@ -0,0 +1,33 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseLinkedList(self, head):
+        curr = head
+        prev = None
+
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        return prev
+
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        # approach: rever the linkedlist
+        # remove the nth element
+        # reverse it back
+        # return the head of the reversed reversed linkedlist (aka just the normal original linkedlist)
+
+        rev = self.reverseLinkedList(head)
+        dummy = ListNode(0, rev)
+        prev = dummy
+        for i in range(n-1):
+            prev = prev.next
+        prev.next = prev.next.next
+
+        return self.reverseLinkedList(dummy.next)
+
+
