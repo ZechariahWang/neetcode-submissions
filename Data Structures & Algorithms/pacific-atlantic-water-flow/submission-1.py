16153dfa3b726e45787bbb7f0e884b3b20bca8c9
Zechariah Wang <97078224+ZechariahWang@users.noreply.github.com>
2026-08-19 17:44:09 -0400

    Add: pacific-atlantic-water-flow - submission-1

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-1.py
@@ -0,0 +1,56 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        # approach: use dfs
+        # make a visited set
+        # loop through every grid, and then check the neighboring nodes values
+        # add node to visited set
+        # if they are <= the current node val, they are still valid so continue
+        # else, if the neighbor node is greater than the current node val, stop exploring as it definitely not valid
+        # continue this loop until its able to find one node that branches out into either the top left side AND another node the bottom and right side
+        # if r < 0 or c < 0, then it went into the pacific, if r>= len(ROWS) or c>= len(COLS), its in the atlantic
+        # Once you are able to satisfy this, then that means u have a valid path, therefore this node is valid
+        # return hit_pacific=True, hit_atlantic=True inside the DFS call
+        # outside the dfs loop, loop through every r in rows
+        # inside that loop, loop through every c in cols
+        # if dfs(r,c, hit_pacific, hit_atlantic) is valid, stores its [r, c] values, append it to res list
+        # outside the two loops, return res
+
+        ROWS = len(heights)
+        COLS = len(heights[0])
+        res = []
+
+        def dfs(r, c, prev_height, visit_set):
+            nonlocal hit_pacific
+            nonlocal hit_atlantic
+
+            if r < 0 or c < 0:
+                hit_pacific = True
+                return True
+            if r >= ROWS or c >= COLS:
+                hit_atlantic = True
+                return True
+
+            if (r, c) in visit_set:
+                return False
+
+            if heights[r][c] > prev_height:
+                return False
+
+            visit_set.add((r, c))
+
+            dfs(r+1,c, heights[r][c], visit_set)
+            dfs(r,c+1, heights[r][c], visit_set)
+            dfs(r-1,c, heights[r][c], visit_set)
+            dfs(r,c-1, heights[r][c], visit_set)
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                hit_pacific = False
+                hit_atlantic = False
+                valid_node = dfs(r, c, heights[r][c], set())
+                if hit_pacific and hit_atlantic:
+                    res.append([r, c])
+
+        return res
+
+        
