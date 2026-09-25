8911caaaeb0fed13cffb2b905843015686764237
Zechariah Wang <97078224+ZechariahWang@users.noreply.github.com>
2026-09-25 12:21:54 -0400

    Add: top-k-elements-in-list - submission-11

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-11.py
@@ -0,0 +1,26 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # count dict fornumber of times a number appears
+        # freq list of lists for an inde with a freq and a value of lists with all numbers that occur that amount of times
+        # run bucket sort, find the ones at the very end of the list
+
+
+        count = {}
+        freq = [[] for i in range(len(nums)+1)]
+
+        for n in nums:
+            count[n] = count.get(n, 0) + 1
+
+        for c, n in count.items():
+            freq[n].append(c)
+
+        res = []
+        for i in range(len(freq)-1,0,-1):
+            for n in freq[i]:
+                if len(res) == k:
+                    return res
+                res.append(n)
+
+        return res
+
+        
