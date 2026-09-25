7d013ad3417ef5eff2f3af879c4fbff4f34a8848
Zechariah Wang <97078224+ZechariahWang@users.noreply.github.com>
2026-09-25 13:03:43 -0400

    Add: is-palindrome - submission-13

---
+++ Data Structures & Algorithms/is-palindrome/submission-13.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        i = 0
+        j = len(s) - 1
+
+        while i < j:
+            # go from left side and clear all alpha nums
+            while i < j and not s[i].isalnum():
+                i += 1
+            # go from right side and clear all alpha nums
+            while j > i and not s[j].isalnum():
+                j -=1
+            # check if currnt char from l == current char from r
+            if s[i].lower() != s[j].lower():
+                return False
+            # if it is increase l and ddecrease r, else return false
+            i += 1
+            j -= 1
+        return True
+
+            # "c".isalnum()
+
+        # return true
